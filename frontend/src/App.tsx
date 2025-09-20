496c7710f09b3f7419a189556e3661038609fabb
wang seong min <163216266+wang-arthur-dev@users.noreply.github.com>
2025-09-20 17:59:59 +0900

    feat: 레이아웃 재구성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/Home/HomePage';
-import { ReportPage } from './pages/Report/ReportPage';
-import MissingPersonListPage from './pages/MissingPersonList/MissingPersonListPage';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/missing-list" element={<MissingPersonListPage />} />
-        <Route path="/report/:missingPersonId" element={<ReportPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/*" element={<HomePage />} />
       </Routes>
     </Router>
   );
