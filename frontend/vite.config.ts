b6c7f658eb6a942f9591855c2d6b9a50f48513ec
EC2 Default User <ec2-user@ip-172-31-8-182.us-west-1.compute.internal>
2025-09-21 06:09:42 +0000

    chmod: init

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,8 @@ export default defineConfig({
     allowedHosts: [
       'localhost',
       '127.0.0.1',
-      'ec2-13-52-103-155.us-west-1.compute.amazonaws.com'
+      'ec2-13-52-103-155.us-west-1.compute.amazonaws.com',
+      'ec2-54-193-170-230.us-west-1.compute.amazonaws.com'
     ]
   }
 })
